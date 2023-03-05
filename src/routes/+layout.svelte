02addfc237d9b6e9d59c37e3ed533aca2b33ff0d
kalucky0 <kalucki23@gmail.com>
2023-03-05 13:39:11 +0100

    Install Material Design Icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,15 @@
 <script>
     import "../app.css";
+    import DotsVertical from 'svelte-material-icons/DotsVertical.svelte';
 </script>
 
-<slot />
+<nav class="toolbar">
+    <p>Test</p>
+    <DotsVertical />
+</nav>
+<main>
+    <slot />
+</main>
+<div class="bottom-navigation">
+
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<div class="">
+	
+</div>
 
 <style lang="postcss">
 	:global(html) {
-		background-color: theme(colors.gray.100);
+		background-color: #000;
 	}
 </style>
